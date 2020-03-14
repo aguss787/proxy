da1e06ef2a852976caa4b885eb06a4f05c58924c
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2020-03-14 19:02:42 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::convert::Infallible;
+use std::net::{SocketAddr};
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Client, Server};
+
+type HttpClient = Client<hyper::client::HttpConnector>;
+
+mod proxy;
+
+// To try this example:
+// 1. cargo run --example http_proxy
+// 2. config http_proxy in command line
+//    $ export http_proxy=http://127.0.0.1:8100
+//    $ export https_proxy=http://127.0.0.1:8100
+// 3. send requests
+//    $ curl -i https://www.some_domain.com/
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3128));
+    let client = HttpClient::new();
+
+    let make_service = make_service_fn(move |_| {
+        let client = client.clone();
+        async move { Ok::<_, Infallible>(service_fn(move |req| proxy::proxy(client.clone(), req))) }
+    });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    println!("Listening on http://{}", addr);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
